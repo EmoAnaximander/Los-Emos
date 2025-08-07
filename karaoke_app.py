bfd96d601b42aba7000994d4a4b6561334158f23
EmoAnaximander <jjeannehoffman@gmail.com>
2025-08-06 17:24:56 -0700

    Add files via upload

--- karaoke_app.py
+++ karaoke_app.py
@@ -1,58 +1,50 @@
 import streamlit as st
 from datetime import datetime
 
-# --- SONG LIST FROM FILE ---
+# --- SONG LIST from "Second Try Correct Songs.xlsx" (42 songs) ---
 SONG_LIST = [
-    "Mayday Parade - Jamie All Over",
-    "Sugarcult - Memory",
-    "Simple Plan - I'd Do Anything",
-    "Brand New - The Quiet Things That No One Ever Knows",
-    "Say Anything - Wow, I Can Get Sexual Too",
-    "Good Charlotte - Girls & Boys",
-    "The Story So Far - Empty Space",
-    "Brand New - Mix Tape",
-    "Something Corporate - Punk Rock Princess",
-    "Hawthorne Heights - Ohio Is for Lovers",
-    "Paramore - Misery Business",
-    "Taking Back Sunday - Cute Without the 'E' (Cut from the Team)",
-    "Fall Out Boy - Sugar, We're Goin Down",
-    "Motion City Soundtrack - Everything Is Alright",
+    "Alkaline Trio - Stupid Kid",
     "All Time Low - Dear Maria, Count Me In",
-    "A Day to Remember - If It Means a Lot to You",
-    "Dashboard Confessional - Screaming Infidelities",
-    "The Used - The Taste of Ink",
-    "Blink-182 - I Miss You",
+    "Avril Lavigne - Sk8er Boi",
+    "Blink-182 - All the Small Things",
+    "Blink-182 - What's My Age Again?",
+    "Bowling for Soup - 1985",
+    "Brand New - Mix Tape",
+    "Brand New - The Quiet Things That No One Ever Knows",
+    "Dashboard Confessional - Vindicated",
+    "Fall Out Boy - Dance Dance",
+    "Fall Out Boy - Sugar, We're Goin Down",
+    "Good Charlotte - Girls & Boys",
+    "Good Charlotte - The Anthem",
+    "Hawthorne Heights - Ohio Is for Lovers",
     "Jimmy Eat World - The Middle",
-    "The Ataris - In This Diary",
-    "Panic! At The Disco - I Write Sins Not Tragedies",
-    "Yellowcard - Ocean Avenue",
-    "Pierce the Veil - King for a Day",
+    "Mayday Parade - Jamie All Over",
+    "My Chemical Romance - Helena",
     "My Chemical Romance - I'm Not Okay (I Promise)",
-    "Green Day - Basket Case",
-    "AFI - Miss Murder",
-    "The All-American Rejects - Swing, Swing",
-    "Sum 41 - Fat Lip",
-    "The Red Jumpsuit Apparatus - Face Down",
-    "Boys Like Girls - The Great Escape",
-    "Cute Is What We Aim For - The Curse of Curves",
-    "We The Kings - Check Yes Juliet",
-    "Cartel - Honestly",
-    "The Academy Is... - About a Girl",
     "New Found Glory - My Friends Over You",
-    "Relient K - Be My Escape",
-    "Escape the Fate - Situations",
-    "Senses Fail - Buried a Lie",
-    "Armor for Sleep - Car Underwater",
-    "Matchbook Romance - My Eyes Burn",
-    "The Starting Line - Best of Me",
-    "Anberlin - Feel Good Drag",
-    "Plain White T's - Hate (I Really Don't Like You)",
-    "The Maine - Into Your Arms",
-    "Forever the Sickest Kids - Woah Oh! (Me vs. Everyone)",
-    "Red Hot Chili Peppers - Dani California",
-    "The Killers - Mr. Brightside",
+    "New Found Glory - Understatement",
+    "Panic! At The Disco - Lying Is the Most Fun a Girl Can Have Without Taking Her Clothes Off",
+    "Papa Roach - Last Resort",
+    "Paramore - Misery Business",
+    "Paramore - Still Into You",
     "Paramore - That's What You Get",
-    "Fountains of Wayne - Stacy's Mom"
+    "Saves The Day - My Sweet Fracture",
+    "Say Anything - Wow, I Can Get Sexual Too",
+    "Simple Plan - I'd Do Anything",
+    "Something Corporate - Punk Rock Princess",
+    "Story of the Year - Until the Day I Die",
+    "Sugarcult - Memory",
+    "Taking Back Sunday - Cute Without the 'E' (Cut From the Team)",
+    "Taking Back Sunday - MakeDamnSure",
+    "The All-American Rejects - Dirty Little Secret",
+    "The Starting Line - The Best of Me",
+    "The Story So Far - Empty Space",
+    "The Story So Far - Roam",
+    "The Used - Buried Myself Alive",
+    "The Used - The Taste of Ink",
+    "Wheatus - Teenage Dirtbag",
+    "Yellowcard - Ocean Avenue",
+    "Yellowcard - Only One"
 ]
 
 # --- SESSION STATE ---
@@ -104,7 +96,7 @@ st.subheader("🔐 Host Controls")
 with st.expander("Enter Host PIN to Access Singer Queue", expanded=False):
     pin_input = st.text_input("Enter host PIN", type="password")
     if st.button("Unlock Controls"):
-        if pin_input == "gibsons2025":  # <-- You can change this
+        if pin_input == "gibsons2025":  # Change PIN if needed
             st.session_state.host_verified = True
             st.success("Host controls unlocked!")
         else:
@@ -122,7 +114,6 @@ if st.session_state.host_verified:
         if not remaining:
             st.info("No more singers in the queue.")
         else:
-            # Sort by earliest signup
             next_song, info = sorted(remaining.items(), key=lambda x: x[1]["timestamp"])[0]
             st.session_state.called_queue.append((next_song, info["name"]))
             st.success(f"🎤 {info['name']}, you're up! Get ready to sing **{next_song}**.")
