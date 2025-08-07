85804f33825077035a42372d3de1dabe73739355
EmoAnaximander <jjeannehoffman@gmail.com>
2025-08-07 10:16:14 -0700

    Add files via upload

--- karaoke_app.py
+++ karaoke_app.py
@@ -7,7 +7,22 @@ from google.oauth2.service_account import Credentials
 # Google Sheets setup
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1JGAubxB_3rUvTdi7XlhHOguWyvuw_37igxRNBz_KDm8/edit"
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-CREDS = Credentials.from_service_account_file("service_account.json", scopes=SCOPES)
+import os
+import json
+
+if "GOOGLE_CREDENTIALS" in st.secrets:
+    # Running on Streamlit Cloud
+    CREDS = Credentials.from_service_account_info(
+        json.loads(st.secrets["GOOGLE_CREDENTIALS"]),
+        scopes=SCOPES
+    )
+else:
+    # Running locally
+    CREDS = Credentials.from_service_account_file(
+        "service_account.json",
+        scopes=SCOPES
+    )
+
 client = gspread.authorize(CREDS)
 sheet = client.open_by_url(SHEET_URL).worksheet("Signups")
 
