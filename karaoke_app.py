783c36eaba95b46e300d6d0f83a28e16b228edc5
EmoAnaximander <jjeannehoffman@gmail.com>
2025-08-14 13:14:32 -0700

    Add files via upload

--- karaoke_app.py
+++ karaoke_app.py
@@ -64,11 +64,11 @@ logo = Image.open(logo_path)
 col1, col2, col3 = st.columns([3, 4, 3])
 with col2:
     st.image(logo, width=300)
-st.markdown("<h1 style='text-align: center;'>Singer Signup</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Singer Signup</h1>
+<p style='text-align: center; font-size: 16px;'>One song per person. Once it's claimed, it disappears. We'll call your name when it's your turn to scream.</p>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; font-size: 16px;'><a href='https://instagram.com/losemoskaraoke' target='_blank' style='color: white; text-decoration: underline;'>Follow us on Instagram</a></p>", unsafe_allow_html=True)
 
 # --- Signup Form ---
-st.subheader("📝 Sign Up to Sing")
 with st.form("signup_form"):
     name = st.text_input("Your name")
     phone_raw = st.text_input("Your phone number (10 digits)")
