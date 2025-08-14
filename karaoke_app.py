dfa1c82ba8b217408ddc3561752aafa5aec3ef40
EmoAnaximander <jjeannehoffman@gmail.com>
2025-08-14 12:46:10 -0700

    Add files via upload

--- karaoke_app.py
+++ karaoke_app.py
@@ -61,9 +61,11 @@ from pathlib import Path
 logo_path = Path(__file__).parent / "logo.png"
 logo = Image.open(logo_path)
 
-col1, col2, col3 = st.columns([3, 4, 3])
-with col2:
-    st.image(logo, width=300)
+st.markdown("""
+    <div style='display: flex; justify-content: center;'>
+        <img src='logo.png' style='max-width: 300px; width: 100%; height: auto;'>
+    </div>
+""", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center;'>Singer Signup</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; font-size: 16px;'><a href='https://instagram.com/losemoskaraoke' target='_blank' style='color: white; text-decoration: underline;'>Follow us on Instagram</a></p>", unsafe_allow_html=True)
 
